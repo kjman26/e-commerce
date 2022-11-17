94e077fb0c91c3ea32cbdcccf5f6edffbdea8b5c
Chris <kjman26@gmail.com>
2022-11-16 17:55:24 -0800

    updated connection and started working the category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,12 +5,50 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    attributes: ["id", 'category_name'],
+    include: [
+      {
+        model: Product,
+        attrictures: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ],
+  })
   // be sure to include its associated Products
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json(err)
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ],
+  })
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res
+      .status(404)
+      .json({ message: "no categories found, try a different ID!"});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
