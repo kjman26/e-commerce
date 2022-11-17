a36a2f6b09b6e71229ce5c8924280dd806e315db
Chris <kjman26@gmail.com>
2022-11-16 18:09:01 -0800

    finished building out get, put, and delete on category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,14 +53,61 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res.status(500).json({ message: "No matching IDs"});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'There are no matching categories with this ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+  
 });
+
 
 module.exports = router;
